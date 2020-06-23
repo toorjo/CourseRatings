1837caaeb9dc2c9c44fcffbc5dad35c4113bae09
Andrew Liu <aliu5454@gmail.com>
2020-06-22 21:11:58 -0700

    Add files via upload

---
+++ rmc.js
@@ -0,0 +1,54 @@
+let doc = document.querySelector(".course");
+
+function getCourses (faculty) {
+    let courseList = [];
+
+fetch("https://ubc-courses-api.herokuapp.com/tree/2018W/" + faculty).then (response => {
+    return response.json();
+    }).then(data => {
+        return data;
+    }).then(data => {
+        // console.log(data);
+        courseList = data.courses[0];
+        // console.log(commCourses);
+        return Object.entries(courseList);
+    }).then(courses => {
+        // console.log(courses);
+        doc.innerHTML += `<h1>${faculty}</h1>`
+        courseList = courses;
+        courseList.forEach(course => {
+        // console.log(course);
+        let courseName = course[1].course_name;
+        let courseTitle = course[1].course_title;
+        // console.log(courseName, courseTitle);
+        doc.innerHTML += `<p>${courseName}: ${courseTitle} </p>`
+        })
+    })
+};
+
+let facultyList = [];
+getCourses("COMM");
+
+// return all the Faculty codes and get their courses
+// fetch("https://ubc-courses-api.herokuapp.com/2018W").then (response => {
+//     return response.json();
+// }).then(data => {
+//     // console.log(data);
+//     return data;
+// }).then(data => {
+//     data.forEach(fac => {
+//         facultyList.push(fac.code);
+//     })
+//     return facultyList;
+// }).then(list => {
+//     list.forEach(faculty => {
+//         getCourses(faculty);
+//     })
+// })
+// console.log(facultyList);
+
+
+// facultyList.forEach(faculty => {
+//     console.log(faculty);
+//     getCourses(faculty);
+// });
